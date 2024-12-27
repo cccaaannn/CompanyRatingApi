80b9f2a1155fa52486b6fe39905af3611d6ea8df
cccaaannn <can.kurt.aa@gmail.com>
2024-12-27 23:51:51 +0300

    injectable attribute

---
+++ src/Shared/Attributes/Injectable/InjectableAttribute.cs
@@ -0,0 +1,12 @@
+namespace CompanyRateApi.Shared.Attributes.Injectable;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false)]
+public class InjectableAttribute : Attribute
+{
+    public ServiceLifetime Lifetime { get; }
+
+    public InjectableAttribute(ServiceLifetime lifetime = ServiceLifetime.Scoped)
+    {
+        Lifetime = lifetime;
+    }
+}
---
+++ src/Shared/Attributes/Injectable/InjectableAttributeServiceExtension.cs
@@ -0,0 +1,24 @@
+using System.Reflection;
+
+namespace CompanyRateApi.Shared.Attributes.Injectable;
+
+public static class InjectableAttributeServiceExtension
+{
+    public static void AddInjectablesFromAssembly(this IServiceCollection services, Assembly assembly)
+    {
+        var typesWithAttribute = assembly.GetTypes()
+            .Where(t => t.GetCustomAttribute<InjectableAttribute>() != null);
+
+        foreach (var type in typesWithAttribute)
+        {
+            var attribute = type.GetCustomAttribute<InjectableAttribute>();
+            _ = attribute!.Lifetime switch
+            {
+                ServiceLifetime.Singleton => services.AddSingleton(type),
+                ServiceLifetime.Scoped => services.AddScoped(type),
+                ServiceLifetime.Transient => services.AddTransient(type),
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+    }
+}
