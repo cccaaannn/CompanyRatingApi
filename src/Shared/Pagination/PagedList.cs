911d87a17e41b3e6da7929f1b6366515de7416ee
cccaaannn <can.kurt.aa@gmail.com>
2024-12-29 02:28:16 +0300

    paged list

---
+++ src/Shared/Pagination/PagedList.cs
@@ -0,0 +1,11 @@
+﻿namespace CompanyRateApi.Shared.Pagination;
+
+public class PagedList<T>(List<T> content, int page, int size, int total)
+{
+    public List<T> Content { get; } = content;
+    public int Page { get; } = page;
+    public int Size { get; } = size;
+    public int Total { get; } = total;
+    public bool HasNext => Page * Size < Total;
+    public bool HasPrev => Page > 1;
+}
