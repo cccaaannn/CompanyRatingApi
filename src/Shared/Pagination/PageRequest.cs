e75cc7c7ac6a05e7a09668e442a4ae9d664231b3
cccaaannn <can.kurt.aa@gmail.com>
2024-12-29 02:28:25 +0300

    page request

---
+++ src/Shared/Pagination/PageRequest.cs
@@ -0,0 +1,9 @@
+namespace CompanyRateApi.Shared.Pagination;
+
+public record PageRequest
+{
+    public int Page { get; init; } = 1;
+    public int Size { get; init; } = 10;
+    public string? SortBy { get; init; }
+    public SortDirection? SortDirection { get; init; }
+}
---
+++ src/Shared/Pagination/SortDirection.cs
@@ -0,0 +1,7 @@
+namespace CompanyRateApi.Shared.Pagination;
+
+public enum SortDirection
+{
+    Ascending,
+    Descending
+}
