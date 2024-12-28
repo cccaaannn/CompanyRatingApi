a1c10cb86e879e88996c0c19d7e81813f4825230
cccaaannn <can.kurt.aa@gmail.com>
2024-12-29 02:28:31 +0300

    page extensions

---
+++ src/Shared/Pagination/PagedListExtensions.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyRateApi.Shared.Pagination;
+
+public static class PagedListExtensions
+{
+    public static async Task<PagedList<T>> ToPagedListAsync<T>(
+        this IQueryable<T> query,
+        int page,
+        int size,
+        CancellationToken cancellationToken
+    )
+    {
+        var total = await query.CountAsync(cancellationToken);
+
+        var items = await query
+            .Skip((page - 1) * size)
+            .Take(size)
+            .ToListAsync(cancellationToken);
+
+        return new PagedList<T>(items, page, size, total);
+    }
+}
