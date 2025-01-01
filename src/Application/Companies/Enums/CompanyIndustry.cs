f6e9b98691260de38f155a049325e7e6daadad4b
cccaaannn <can.kurt.aa@gmail.com>
2025-01-01 17:59:15 +0300

    enums

---
+++ src/Application/Companies/Enums/CompanyIndustry.cs
@@ -0,0 +1,12 @@
+namespace CompanyRateApi.Application.Companies.Enums;
+
+public enum CompanyIndustry
+{
+    Technology,
+    Finance,
+    Health,
+    Education,
+    Food,
+    Retail,
+    Other
+}
