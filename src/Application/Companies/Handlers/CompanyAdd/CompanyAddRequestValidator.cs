fbd99f282e115f89d344f3b0e84e41f69df4d07d
cccaaannn <can.kurt.aa@gmail.com>
2025-12-08 02:56:25 +0300

    validations fixed

--- src/Application/Companies/Handlers/CompanyAdd/CompanyAddRequestValidator.cs
+++ src/Application/Companies/Handlers/CompanyAdd/CompanyAddRequestValidator.cs
@@ -6,31 +6,31 @@ public class CompanyAddRequestValidator : AbstractValidator<CompanyAddRequest>
 {
     public CompanyAddRequestValidator()
     {
-        RuleFor(x => x.Name).NotEmpty().MaximumLength(100);
+        RuleFor(x => x.Name).NotEmpty().MaximumLength(1000);
         
         RuleFor(x => x.Industry).IsInEnum();
         
-        RuleFor(x => x.Description).MaximumLength(500);
+        RuleFor(x => x.Description).MaximumLength(5000);
         
-        RuleFor(x => x.Address).MaximumLength(500);
+        RuleFor(x => x.Address).MaximumLength(1000);
         
-        RuleFor(x => x.City).MaximumLength(100);
+        RuleFor(x => x.City).MaximumLength(500);
         
-        RuleFor(x => x.Country).MaximumLength(100);
+        RuleFor(x => x.Country).MaximumLength(500);
         
         RuleFor(x => x.Email)
             .NotEmpty()
-            .EmailAddress().MaximumLength(100)
+            .EmailAddress().MaximumLength(1000)
             .When(x => !string.IsNullOrEmpty(x.Email));
         
         RuleFor(x => x.Website)
             .NotEmpty()
-            .MaximumLength(100).Must(uri => Uri.IsWellFormedUriString(uri, UriKind.Absolute))
+            .MaximumLength(1000).Must(uri => Uri.IsWellFormedUriString(uri, UriKind.Absolute))
             .When(x => !string.IsNullOrEmpty(x.Website));
         
         RuleFor(x => x.LogoUrl)
             .NotEmpty()
-            .MaximumLength(100).Must(uri => Uri.IsWellFormedUriString(uri, UriKind.Absolute))
+            .MaximumLength(1000).Must(uri => Uri.IsWellFormedUriString(uri, UriKind.Absolute))
             .When(x => !string.IsNullOrEmpty(x.LogoUrl));
     }
 }
